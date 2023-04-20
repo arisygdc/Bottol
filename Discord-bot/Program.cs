4320557a94a1efb5e8ca847cc9df974b86f75ca8
arisygdc <arisycso@gmail.com>
2023-04-20 23:24:55 +0700

    initialize bot context

--- Discord-bot/Program.cs
+++ Discord-bot/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+public class Program
+{
+    private DiscordSocketClient _client;
+    private CommandService _commands;
+
+    public static void Main(string[] args) =>
+        new Program().MainAsync().GetAwaiter().GetResult();
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        _commands = new CommandService();
+
+        var token = "token";
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+}
